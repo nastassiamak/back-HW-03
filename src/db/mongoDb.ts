5e6f468c8a8b268a02fffb540e2b666d0a2902ad
«nastassiamak» <redfox.asya@gmail.com>
2025-03-17 15:24:31 +0300

    update 20

--- src/db/mongoDb.ts
+++ src/db/mongoDb.ts
@@ -61,21 +61,6 @@ export async function runDb(url: string): Promise<boolean> {
         return false;
     }
 }
-export async function clearDb(): Promise<void> {
-    try {
-        if (blogsCollection) {
-            await blogsCollection.deleteMany({}); // Удаляем все документы из коллекции блогов
-            console.log("Все блоги были удалены из коллекции.");
-        }
-
-        if (postsCollection) {
-            await postsCollection.deleteMany({}); // Удаляем все документы из коллекции постов
-            console.log("Все посты были удалены из коллекции.");
-        }
-    } catch (err) {
-        console.error("Ошибка при очистке базы данных:", err);
-    }
-}
 
 
 // Функция для отключения от базы данных
--- src/features/testing/index.ts
+++ src/features/testing/index.ts
@@ -1,12 +1,24 @@
 import {Router} from "express";
-import {HTTP_STATUSES, setDB} from "../../db/db";
-import {clearDb} from "../../db/mongoDb";
+import {blogsCollection, postsCollection} from "../../db/mongoDb";
 
 export const testingRouter = Router();
 
 testingRouter.delete('/all-data', async (req,
                                          res) => {
-    setDB()
-    res.status(HTTP_STATUSES.NO_CONTENT_204).json({});
+
+        try {
+            if (blogsCollection) {
+                await blogsCollection.deleteMany({}); // Удаляем все документы из коллекции блогов
+                console.log("Все блоги были удалены из коллекции.");
+            }
+
+            if (postsCollection) {
+                await postsCollection.deleteMany({}); // Удаляем все документы из коллекции постов
+                console.log("Все посты были удалены из коллекции.");
+            }
+        } catch (err) {
+            console.error("Ошибка при очистке базы данных:", err);
+        }
+
 
 })
