cd641018f969df60b1869b81dad32ab313369ffd
«nastassiamak» <redfox.asya@gmail.com>
2025-03-27 22:54:29 +0300

    update 30

--- src/features/testing/index.ts
+++ src/features/testing/index.ts
@@ -7,9 +7,11 @@ export const testingRouter = Router();
 testingRouter.delete('/all-data', async (req, res) => {
     // Эндпоинт для удаления всех данных из базы данных
     try {
-        const db = client.db("blogs-platform"); // Получаем базу данных
+        const db = client.db("blogs-platform");
+        await db.command({ ping: 1 }); // Проверка на подключение по выполнению команды ping
         await clearDatabase(db); // Вызываем функцию очистки базы данных
         res.sendStatus(204); // Возвращаем статус 204
+
     } catch (err) {
         console.error("Ошибка при удалении всех данных:", err);
         res.status(500).send("Ошибка сервера"); // Возвращаем статус 500 в случае ошибки
