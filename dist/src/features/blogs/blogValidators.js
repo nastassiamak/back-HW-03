fbe0001d7943873bc3774ec932ecf16d24e9745f
«nastassiamak» <redfox.asya@gmail.com>
2025-03-28 17:13:23 +0300

    update 41

--- dist/src/features/blogs/blogValidators.js
+++ dist/src/features/blogs/blogValidators.js
@@ -9,7 +9,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.blogValidators = exports.findBlogValidator = exports.websiteUrlValidator = exports.descriptionValidator = exports.nameValidator = void 0;
+exports.blogValidators = exports.findBlogValidator = exports.isMembershipValidator = exports.createdAtValidator = exports.websiteUrlValidator = exports.descriptionValidator = exports.nameValidator = void 0;
 const express_validator_1 = require("express-validator");
 const admin_middleware_1 = require("../../global_middlewares/admin-middleware");
 const db_1 = require("../../db/db");
@@ -29,17 +29,15 @@ exports.websiteUrlValidator = (0, express_validator_1.body)('websiteUrl')
     .isString().withMessage('not string')
     .trim().isURL().withMessage('not url')
     .isLength({ min: 1, max: 100 }).withMessage('more then 100 or 0');
-/*
-export const createdAtValidator = body('createdAt')
+exports.createdAtValidator = (0, express_validator_1.body)('createdAt')
     .isString()
     .withMessage('not string')
     .trim()
     .matches(/^\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d(\.\d+)?([+-][0-2]\d:[0-5]\d|Z)$/)
     .withMessage('not valid date format');
-*/
-// export const isMembershipValidator = body('isMembership')
-//     .isBoolean().withMessage('must be a boolean')
-//     .toBoolean(); // Опционально, чтобы преобразовать входное значение в булевый тип
+exports.isMembershipValidator = (0, express_validator_1.body)('isMembership')
+    .isBoolean().withMessage('must be a boolean')
+    .toBoolean(); // Опционально, чтобы преобразовать входное значение в булевый тип
 const findBlogValidator = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     const { id } = req.params;
     if (typeof id !== "string" || id.trim().length === 0) {
@@ -53,7 +51,7 @@ exports.blogValidators = [
     exports.nameValidator,
     exports.descriptionValidator,
     exports.websiteUrlValidator,
-    //createdAtValidator,
-    //isMembershipValidator,
+    exports.createdAtValidator,
+    exports.isMembershipValidator,
     inputCheckErrorsMiddleware_1.inputCheckErrorsMiddleware,
 ];
