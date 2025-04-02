f226f4c44b87c4c0f7c1fcf13e247ddb085d68e4
«nastassiamak» <redfox.asya@gmail.com>
2025-04-02 15:57:08 +0300

    update 65

--- src/features/posts/postValidators.ts
+++ src/features/posts/postValidators.ts
@@ -21,6 +21,15 @@ export const shortDescriptionValidator = body("shortDescription").isString().wit
 export const contentValidator = body("content").isString().withMessage('not string')
     .trim().isLength({min: 1, max: 1000}).withMessage('more then 1000 or 0')
 
+export const createdAtValidator = body('createdAt')
+    .optional() // Делает поле необязательным
+    .isString()
+    .withMessage('not string')
+    .trim()
+    .matches(/^\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d(\.\d+)?([+-][0-2]\d:[0-5]\d|Z)$/)
+    .withMessage('not valid date format');
+
+
 export const blogIdValidator = body("blogId").isString().withMessage('not string')
     .trim()
     .custom(async (blogId: string) => {
@@ -52,6 +61,7 @@ export const postValidators = [
     shortDescriptionValidator,
     contentValidator,
     blogIdValidator,
+    createdAtValidator,
 
     inputCheckErrorsMiddleware
 
